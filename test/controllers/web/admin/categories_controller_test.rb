82e6b3537fbfc9d9b2cdd786a27a9ed8bb345e47
Ruslan Knyazev <agrry@yandex.ru>
2025-12-21 23:12:25 +0500

    add tests for bulletins

---
+++ test/controllers/web/admin/categories_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+# class Web::Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
+#   test "should get index" do
+#     get web_admin_categories_index_url
+#     assert_response :success
+#   end
+#
+#   test "should get show" do
+#     get web_admin_categories_show_url
+#     assert_response :success
+#   end
+#
+#   test "should get new" do
+#     get web_admin_categories_new_url
+#     assert_response :success
+#   end
+#
+#   test "should get create" do
+#     get web_admin_categories_create_url
+#     assert_response :success
+#   end
+#
+#   test "should get edit" do
+#     get web_admin_categories_edit_url
+#     assert_response :success
+#   end
+#
+#   test "should get update" do
+#     get web_admin_categories_update_url
+#     assert_response :success
+#   end
+#
+#   test "should get destroy" do
+#     get web_admin_categories_destroy_url
+#     assert_response :success
+#   end
+# end
--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -1,23 +1,55 @@
 require "test_helper"
 
 class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get web_bulletins_index_url
+  test "#index" do
+    get root_path
     assert_response :success
   end
 
-  test "should get show" do
-    get web_bulletins_show_url
+  test "#show" do
+    user = users(:one)
+    sign_in(user)
+
+    bulletin = bulletins(:bulletin_one)
+    get bulletin_path(bulletin)
     assert_response :success
   end
 
-  test "should get new" do
-    get web_bulletins_new_url
+  test "#show unathorized" do
+    bulletin = bulletins(:bulletin_one)
+    get bulletin_path(bulletin)
     assert_response :success
   end
 
-  test "should get create" do
-    get web_bulletins_create_url
+  test "#new" do
+    user = users(:one)
+    sign_in(user)
+
+    get new_bulletin_path
     assert_response :success
+  end
+
+  test "#new unathorized" do
+    get new_bulletin_path
+    assert_response :redirect
+  end
+
+  test '#create' do
+    user = users(:one)
+    sign_in(user)
+
+    attrs = {
+      bulletin: {
+        title: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph(sentence_count: 10),
+        category_id: Category.first.id,
+      }
+    }
+
+    post bulletins_path, params: attrs
+
+    assert { Bulletin.exists?(title: attrs[:bulletin][:title]) }
+    assert { flash[:notice].present? }
+    assert_response :redirect
   end
 end
